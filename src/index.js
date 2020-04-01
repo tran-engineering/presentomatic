a88d18336d1676e702b8bc2ba7962107ab576263
Khôi Tran <kt@compile.ch>
2020-04-01 12:03:15 +0200

    remove remote control and stuff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import RemoteControl from './model/RemoteControl';
 import html from '../PRESENTATION.md';
 
 const p = new Presentation(html);
-const r = new RemoteControl(window.location.href.replace(window.location.hash, ''));
+/* const r = new RemoteControl(window.location.href.replace(window.location.hash, ''));
 p.addListener('slideChanged', (index) => r.send('slideChanged', index));
-r.addListener('slideChanged', (index) => p.showSlide(index));
+r.addListener('slideChanged', (index) => p.showSlide(index)); */
