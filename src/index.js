21caf86cb9b102fc0957253275d5cc66a58266a9
Khôi Tran <kt@compile.ch>
2025-08-24 23:33:01 +0200

    remove old stuffes

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Presentation from "./model/Presentation";
-import { marked } from "marked";
-const FILE = "PRESENTATION.md";
-
-(async () => {
-    const md = await (await fetch(FILE)).text();
-    const html = marked.parse(md);
-    new Presentation(html);
-  })();
